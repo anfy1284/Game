9df0af9c78c5c609b1d3b41b8b4596a4367ef100
anfy <anfy1284@gmail.com>
2020-10-17 14:45:30 +0300

    Initial commit

---
+++ android/src/ru/anfy/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.anfy.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.anfy.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
